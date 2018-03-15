8e662b69f230581bad5008b9cff80a7daadc0631
MaxHodgson <36774306+MaxHodgson@users.noreply.github.com>
2018-03-15 00:20:58 +0000

    Add files via upload

---
+++ Utility.java
@@ -0,0 +1,103 @@
+package a40274665.calucaltor;
+
+/**
+ * Created by Hodgson on 12/03/2018.
+ */
+
+public final class Utility {
+    private Utility(){
+
+    }
+
+    public static boolean isOperator(char c){
+        return c == '/' ||
+                c == '÷' ||
+                c == '*' ||
+                c == '-' ||
+                c == '+';
+    }
+
+    public static double evaluate(final String str){
+        class Parser {
+            int pos = -1, c;
+
+            void eatChar() {
+                c = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            void eatSpace() {
+                while (Character.isWhitespace(c)) eatChar();
+            }
+
+            double parse() {
+                eatChar();
+                double v = parseExpression();
+                if (c != -1) throw new RuntimeException("Unexpected: " + (char)c);
+                return v;
+            }
+
+
+            double parseExpression() {
+                double v = parseTerm();
+                for (;;) {
+                    eatSpace();
+                    if (c == '+') { // addition
+                        eatChar();
+                        v += parseTerm();
+                    } else if (c == '-') { // subtraction
+                        eatChar();
+                        v -= parseTerm();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseTerm() {
+                double v = parseFactor();
+                for (;;) {
+                    eatSpace();
+                    if (c == '/' || c == '÷') { // division
+                        eatChar();
+                        v /= parseFactor();
+                    } else if (c == '*' || c == '(') { // multiplication
+                        if (c == '*') eatChar();
+                        v *= parseFactor();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseFactor() {
+                double v;
+                boolean negate = false;
+                eatSpace();
+                if (c == '+' || c == '-') {
+                    negate = c == '-';
+                    eatChar();
+                    eatSpace();
+                }
+                if (c == '(') { // brackets
+                    eatChar();
+                    v = parseExpression();
+                    if (c == ')') eatChar();
+                } else { // numbers
+                    int startIndex = this.pos;
+                    while ((c >= '0' && c <= '9') || c == '.') eatChar();
+                    if (pos == startIndex) throw new RuntimeException("Unexpected: " + (char)c);
+                    v = Double.parseDouble(str.substring(startIndex, pos));
+                }
+
+                eatSpace();
+                if (c == '^') { // exponentiation
+                    eatChar();
+                    v = Math.pow(v, parseFactor());
+                }
+                if (negate) v = -v;
+                return v;
+            }
+        }
+        return new Parser().parse();
+    }
+}
